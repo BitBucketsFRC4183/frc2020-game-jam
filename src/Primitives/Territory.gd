7a6759c23270407b09ad588a2f9e07e246217a78
Craig Post <cpost@bitbuckets.org>
2020-11-08 09:59:31 -0700

    Added Territory component
    The Territory is an Area2D built from polygons

---
+++ src/Primitives/Territory.gd
@@ -0,0 +1,42 @@
+extends CollisionPolygon2D
+
+export var color: Color = Color.black
+export var highlight_color: Color = Color.lightgray
+
+signal area_entered(area)
+signal area_exited(area)
+signal mouse_entered(area)
+signal mouse_exited(area)
+
+onready var area2d = $Area2D
+
+func _ready():
+	$Polygon2D.polygon = polygon
+	$Polygon2D.color = color
+	
+	call_deferred("_reparent_area2d")
+
+func _reparent_area2d():
+	# make this area2d our parent so we can create a polygon2d in the editor, but 
+	# use an Area2D with a polygon shape and collision box in game
+	remove_child(area2d)
+	get_parent().add_child(area2d)
+	
+	get_parent().remove_child(self)
+	area2d.add_child(self)
+
+
+# Pass along some area2d signals to the parent
+func _on_Area2D_mouse_entered():
+	$Polygon2D.color = highlight_color
+	emit_signal("mouse_entered", area2d)
+
+func _on_Area2D_mouse_exited():
+	$Polygon2D.color = color
+	emit_signal("mouse_exited", area2d)
+
+func _on_Area2D_area_entered(area):
+	emit_signal("area_entered", area)
+
+func _on_Area2D_area_exited(area):
+	emit_signal("area_exited", area)
