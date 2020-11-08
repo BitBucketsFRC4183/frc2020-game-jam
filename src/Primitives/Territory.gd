42369511b5d6cb725e97eac51a839b430990b99b
Elias Post <elias@postfamilydigital.net>
2020-11-08 12:28:42 -0700

    Added the map

--- src/Primitives/Territory.gd
+++ src/Primitives/Territory.gd
@@ -1,6 +1,8 @@
+tool
+
 extends CollisionPolygon2D
 
-export var color: Color = Color.black
+export var color: Color = Color.black setget set_color
 export var highlight_color: Color = Color.lightgray
 
 onready var area2d = $Area2D
@@ -9,7 +11,8 @@ func _ready():
 	$Polygon2D.polygon = polygon
 	$Polygon2D.color = color
 	
-	call_deferred("_reparent_area2d")
+	if not Engine.editor_hint:
+		call_deferred("_reparent_area2d")
 
 func _reparent_area2d():
 	# make this area2d our parent so we can create a polygon2d in the editor, but 
@@ -20,6 +23,9 @@ func _reparent_area2d():
 	get_parent().remove_child(self)
 	area2d.add_child(self)
 
+func set_color(value):
+	color = value
+	$Polygon2D.color = color
 
 # Pass along some area2d signals to the parent
 func _on_Area2D_mouse_entered():
