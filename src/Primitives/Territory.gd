a5485c4362cf7f35fb7f32c20458de6866b2d57f
Elias Post <elias@postfamilydigital.net>
2020-11-08 17:42:16 -0700

    Map changes and (currently unusable) GUI
    -Territories have integers to see who owns them
    -Territories have types (normal, resource, destroyed)
    -There are now 5 countries on the map
    -Added a HUD that refuses to cooperate

--- src/Primitives/Territory.gd
+++ src/Primitives/Territory.gd
@@ -2,14 +2,27 @@ tool
 
 extends CollisionPolygon2D
 
+enum territory_type {normal, resource, destroyed}
+
+export var territory_owner: int = 0 setget set_territory_owner
 export var color: Color = Color.black setget set_color
 export var highlight_color: Color = Color.lightgray
+export(territory_type) var type = territory_type.normal setget set_type
 
 onready var area2d = $Area2D
 
+export var player_colors = [
+	Color.black,
+	Color.red,
+	Color.blue,
+	Color.green,
+	Color.purple,
+	Color.aqua
+]
+
 func _ready():
 	$Polygon2D.polygon = polygon
-	$Polygon2D.color = color
+	$Polygon2D.color = get_polygon_color()
 	
 	if not Engine.editor_hint:
 		call_deferred("_reparent_area2d")
@@ -25,11 +38,27 @@ func _reparent_area2d():
 
 func set_color(value):
 	color = value
-	$Polygon2D.color = color
+	$Polygon2D.color = get_polygon_color()
+	
+func set_territory_owner(value):
+	territory_owner = value
+	set_color(player_colors[value])
+	
+func set_type(value):
+	type = value
+	$Polygon2D.color = get_polygon_color()
+	
+func get_polygon_color():
+	if type == territory_type.normal:
+		return color
+	elif type == territory_type.resource:
+		return color.lightened(0.3)
+	elif type == territory_type.destroyed:
+		return Color.orangered
 
 # Pass along some area2d signals to the parent
 func _on_Area2D_mouse_entered():
 	$Polygon2D.color = highlight_color
 
 func _on_Area2D_mouse_exited():
-	$Polygon2D.color = color
+	$Polygon2D.color = get_polygon_color()
