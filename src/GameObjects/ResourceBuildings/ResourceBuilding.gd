9dead355ff4294f464b02f4e59f65bb114a52fa5
Ojas Sanghi <ojas.sanghi@protonmail.com>
2020-11-10 16:55:56 -0700

    Works with defense buildings now

--- src/GameObjects/ResourceBuildings/ResourceBuilding.gd
+++ src/GameObjects/ResourceBuildings/ResourceBuilding.gd
@@ -1,17 +1,11 @@
-#class_name ResourceBuilding
-extends Area2D
+extends GameBuilding
 
 export (Enums.resource_types) var resource_type = Enums.resource_types.EXCEPTION
 export var resource_amt := -1
 
-var placeable := false
-var in_another_area := false
-var newly_spawned := false
-
 func _ready() -> void:
 	Signals.connect("day_passed", self, "_on_day_passed")
-	connect("area_entered", self, "_on_area_entered")
-	connect("area_exited", self, "_on_area_exited")
+	._ready()
 
 func _on_day_passed():
 	assert(resource_type != Enums.resource_types.EXCEPTION)
@@ -19,32 +13,3 @@ func _on_day_passed():
 
 	var resource_info = [resource_type, resource_amt]
 	Signals.emit_signal("resource_generated", resource_info)
-
-func _on_area_entered(area):
-	if not newly_spawned:
-		return
-
-	if area.is_in_group("territories"):
-		check_valid_territory(area)
-	else:
-		in_another_area = true
-		placeable = false
-
-func _on_area_exited(area):
-	if not newly_spawned:
-		return
-
-	if in_another_area:
-		if area.is_in_group("territories"):
-			placeable = false
-		else:
-			in_another_area = false
-			for area in get_overlapping_areas():
-				if area.is_in_group("territories"):
-					check_valid_territory(area)
-
-func check_valid_territory(area):
-	if area.get_child(0).type == Enums.territory_types.resource and not in_another_area:
-		placeable = true
-	else:
-		placeable = false
