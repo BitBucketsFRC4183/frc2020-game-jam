16affbbf0feffbf46854d5c79c5a4eef34f49f58
Saptarshi Mallick <sapmallick@gmail.com>
2020-11-09 21:37:01 -0700

    Added popup opening when you click a Tech node

--- src/GameObjects/TechTree.gd
+++ src/GameObjects/TechTree.gd
@@ -17,4 +17,26 @@ func _on_Tech_pressed(tech_name):
 	#	Is tech already unlocked
 	#	Is tech the next tier (can't directly research Tier 3)
 	print(tech_name)
+	
+	var can_research = true
+	var popup: String
+	
+	#Not complete, still testing
+	is_tech_valid(tech_name)
+	
+	if(can_research):
+		popup = "ResearchPopup"
+	else:
+		popup = "ResearchFailedPopup"
+	
+	get_node(popup).set_info(tech_name)
+	get_node(popup).popup_centered()
+	pass
+
+func is_tech_valid(tech):
+	var tech_name = tech.substr(0, tech.length() - 1).to_lower()
+	var tech_num = int(tech.substr(tech.length() - 1))
+	
+	print(tech_name)
+	print(tech_num)
 	pass
