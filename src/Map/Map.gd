f7222c25dd45017eaff6dbacc07583641c7e4bd3
Craig Post <cpost@bitbuckets.org>
2020-11-10 17:18:17 -0700

    Reformatted stuff in the editor

--- src/Map/Map.gd
+++ src/Map/Map.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
-    
+
 func get_territories(root: Node = self) -> Array:
-    # recursively loop through all nodes in the tree and find all the Territories
-    var territories = []
-    for node in root.get_children():
-        if node is Territory:
-            territories.append(node)
-        if node.get_child_count() > 0:
-            var child_territories = get_territories(node)
-            for child_territory in child_territories:
-                territories.append(child_territory)
-    return territories
+	# recursively loop through all nodes in the tree and find all the Territories
+	var territories = []
+	for node in root.get_children():
+		if node is Territory:
+			territories.append(node)
+		if node.get_child_count() > 0:
+			var child_territories = get_territories(node)
+			for child_territory in child_territories:
+				territories.append(child_territory)
+	return territories
