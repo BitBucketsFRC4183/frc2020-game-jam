7e2a7543efdcad4134a565d5d6b4d7075ecc7d50
Ojas Sanghi <ojas.sanghi@protonmail.com>
2020-11-11 11:31:01 -0700

    fix for if we exit a building inside of a resource territory

--- src/GameObjects/GameBuilding.gd
+++ src/GameObjects/GameBuilding.gd
@@ -43,36 +43,42 @@ func _on_area_exited(area):
 	# if we exit a resource territory, we would normally want to set plcaeable to false
 	# but if we entered a new resoruce territory, we don't want to do that
 	# so we check for that
-
 	var child_node = area.get_child(0)
 	# if we just exited a territory
 	if child_node is Territory:
 		# only go ahead if we just exited a resource territory
 		if child_node.type == Enums.territory_types.resource:
-			# get all the areas we're inside of
-			var areas = get_overlapping_areas()
-			# remove the area we just exited from
-			areas.erase(area)
-			for a in areas:
-				var child = a.get_child(0)
-				if child is Territory:
-					# if we're in another resource territory, we're good
-					if child.type == Enums.territory_types.resource:
-						placeable = true
-					# we r in a non-resource territory
-					else:
-						placeable = false
-				# if the area is not a Territory, it's something else
-				# say it's not placeable and quit
-				else:
-					in_non_territory_area = true
-					placeable = false
-					return
-
-			# if the list is empty, we're in the ocean. heck nah
-			if areas.size() == 0:
-				placeable = false
+			validate_new_territory(area)
 	# if we just exited an area and it wasn't a territory
-	# we're probably no longer intneractng with a non-territory area
 	else:
+		# we're probably no longer intneractng with a non-territory area
 		in_non_territory_area = false
+		# but we might have exited the building into a resource territory
+		# so we need to re-validate
+		validate_new_territory(area)
+
+
+func validate_new_territory(area):
+	# get all the areas we're inside of
+	var areas = get_overlapping_areas()
+	# remove the area we just exited from
+	areas.erase(area)
+	for a in areas:
+		var child = a.get_child(0)
+		if child is Territory:
+			# if we're in another resource territory, we're good
+			if child.type == Enums.territory_types.resource:
+				placeable = true
+			# we r in a non-resource territory
+			else:
+				placeable = false
+		# if the area is not a Territory, it's something else
+		# say it's not placeable and quit
+		else:
+			in_non_territory_area = true
+			placeable = false
+			return
+
+	# if the list is empty, we're in the ocean. heck nah
+	if areas.size() == 0:
+		placeable = false
