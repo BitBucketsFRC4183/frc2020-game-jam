3ac0032fb6b6c4edfe3fc7c791b78deac4b5dd1d
Elias Post <elias@postfamilydigital.net>
2020-11-12 13:42:28 -0700

    ignore this

--- src/GameObjects/Asteroids/AsteroidManager.gd
+++ src/GameObjects/Asteroids/AsteroidManager.gd
@@ -7,9 +7,17 @@ export var max_strength = 50
 export var max_count = 50
 export var base_wave_time = 60
 
+var asteroid_small = preload("res://src/GameObjects/Asteroids/FallingAsteroidSmall.tscn")
+var asteroid_medium = preload("res://src/GameObjects/Asteroids/FallingAsteroidMedium.tscn")
+var asteroid_large = preload("res://src/GameObjects/Asteroids/FallingAsteroidLarge.tscn")
+
 var wave = 0
 var asteroid_count = 1
 var territories = []
+var active_asteroids = 0
+
+func _ready():
+	Signals.connect("asteroid_impact", self, "_on_asteroid_impact")
 
 func _on_Timer_timeout():
 	if wave < waves or waves == -1:
@@ -29,10 +37,14 @@ func _on_Timer_timeout():
 		if asteroid_strength > max_strength:
 			asteroid_strength = max_strength
 		if asteroid_strength < 15:
-			asteroid = load("res://src/GameObjects/Asteroids/FallingAsteroidSmall.tscn").instance()
+			asteroid = asteroid_small.instance()
 		elif asteroid_strength < 30:
-			asteroid = load("res://src/GameObjects/Asteroids/FallingAsteroidMedium.tscn").instance()
+			asteroid = asteroid_medium.instance()
 		else:
-			asteroid = load("res://src/GameObjects/Asteroids/FallingAsteroidSmall.tscn").instance()
+			asteroid = asteroid_large.instance()
 		asteroid.global_position = territories[i].center_global
+		active_asteroids += 1
 		add_child(asteroid)
+
+func _on_asteroid_impact(impact_point, explosion_radius):
+	active_asteroids -= 1
