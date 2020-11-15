eb5c08bed278826d551ed6da03a8aed91ef65794
Saptarshi Mallick <sapmallick@gmail.com>
2020-11-15 12:18:53 -0700

    Fixed score being reset before exiting game

--- src/GUI/OpenLeaderboard.gd
+++ src/GUI/OpenLeaderboard.gd
@@ -6,6 +6,10 @@ func _ready():
 	isLeaderboardOpen = false
 	pass
 
+func _input(event):
+	if(event.is_action_pressed("escape") && isLeaderboardOpen):
+		show_leaderboard()
+
 func show_leaderboard():
 	if(!isLeaderboardOpen):
 		get_parent().get_node("Leaderboard").show()
